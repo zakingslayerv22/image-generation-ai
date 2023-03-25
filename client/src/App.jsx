82b1d17b019c02866311bbba2cc3645aa2c8a62c
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-03-25 14:34:05 +0100

    Create a new div to style Home and Create
    New div with flex justify-end items-center gap-2
    to especially to align Home and Create buttons
    to the extreme left (justify-end) with Tailwind
    CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,11 @@ const App = () => (
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
 
+      {/* New div to push the Create and Home to the extreme left and gap */}
+      <div className="flex justify-end items-center gap-2">
+      <Link to="https://corellaprotocol.com" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Home</Link>
       <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      </div>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
